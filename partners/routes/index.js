1249dd1d2ecfd29e17961ab8bc73ad91a8855e3c
Dan Lin <dlin2102@gmail.com>
2017-04-03 17:11:03 -0400

    post page route

--- partners/routes/index.js
+++ partners/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Idea = mongoose.model('Idea');
+var Comment = mongoose.model('Comment')
 
-/* GET home page. */
+/* home page */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* post page */
+router.get('/ideas', function(req, res, next) {
+  Idea.find(function(err, ideas){
+    if(err){ return next(err); }
+
+    res.json(ideas);
+  });
 });
 
 module.exports = router;
