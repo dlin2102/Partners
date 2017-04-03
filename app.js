8716a151526e5c8b7567b068356a29274d2b2226
Dan Lin <dlin2102@gmail.com>
2017-04-03 13:30:13 -0400

    added comments and node backend

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 angular
-.module('partners', ['ui.router']);
+.module('partners', ['ui.router'])
+
 .config([
     "$stateProvider",
     "$urlRouterProvider",
     RouterFunction
 ])
+
 .factory('ideas', [function() {
-  var i = {
-    ideas: [title:'idea',description:'descrip',upvotes:0]
-  };
-  return i;
+  var idea = [
+  ];
+  return idea;
 }])
+
 .controller('MainCtrl', [
     '$scope',
-     ideas,
+     'ideas',
     function($scope, ideas) {
-        $scope.test = 'Hello world!';
-
+        $scope.test = 'Hello world!'
 
         $scope.ideas = ideas.ideas
 
@@ -27,7 +28,11 @@ angular
             $scope.ideas.push({
                 title: $scope.title,
                 description: $scope.description,
-                upvotes: 0
+                upvotes: 0,
+                comments:[
+                  {author: 'Sam', body: 'Great idea!', upvote: 0},
+                  {author: 'Steve', body: "I think that's already a product", upvote: 0}
+                ]
             });
             $scope.title = '';
             $scope.description = '';
@@ -35,18 +40,39 @@ angular
 
         $scope.incrementUpvotes = function(idea) {
             idea.upvotes += 1;
-        };
+        }
 
-    }
+    }])
 
-    function RouterFunction($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state("home", {
-            url: "/home",
-            templateUrl: '/home.html',
-            controller: 'MainCtrl'
-     });
+.controller('IdeasCtrl', ['$scope',
+'$stateParams',
+'ideas',
+function($scope, $stateParams, ideas){
+  $scope.idea = ideas.ideas[$stateParams.id];
 
-    $urlRouterProvider.otherwise('/home');
+  $scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.idea.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+}])
+
+function RouterFunction($stateProvider, $urlRouterProvider) {
+$stateProvider
+    .state("home", {
+        url: "/home",
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+ })
+ .state('ideas', {
+   url: '/ideas/{id}',
+   templateUrl: '/ideas.html',
+   controller: 'IdeasCtrl'
+ })
+
+$urlRouterProvider.otherwise('/home');
 }
-]);
