0ffbf54314cad1c0103570c9183bebf7a18e37f7
Dan Lin <dlin2102@gmail.com>
2017-04-03 20:56:03 -0400

    CR for ideas and comments in index.js

--- partners/routes/index.js
+++ partners/routes/index.js
@@ -9,12 +9,59 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* idea page */
+/* ideas indexpage */
 router.get('/ideas', function(req, res, next) {
   Idea.find(function(err, ideas){
     if(err){ return next(err); }
 
     res.json(ideas);
+  });
+});
+
+/* Using param() to auto load Idea*/
+
+router.param('idea', function(req, res, next, id) {
+  var query = Idea.findById(id);
+
+  query.exec(function (err, idea){
+    if (err) { return next(err); }
+    if (!idea) { return next(new Error('can\'t find idea')); }
+
+    req.idea = idea;
+    return next();
+  });
+});
+
+/* Using param() to auto load Comment*/
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+/*idea showpage */
+router.get('/ideas/:idea', function(req, res, next) {
+  req.idea.populate('comments', function(err, idea) {
+    if (err) { return next(err); }
+
+    res.json(idea);
+  });
+});
+
+
+/*upvote idea */
+router.put('/ideas/:idea/upvote', function(req, res, next) {
+  req.idea.upvote(function(err, idea){
+    if (err) { return next(err); }
+
+    res.json(idea);
   });
 });
 
@@ -29,30 +76,21 @@ router.post('/ideas', function(req, res, next) {
   });
 });
 
-/* Using param() to auto load an object*/
+/* Creating a new comment and saving it to database */
 
-router.param('idea', function(req, res, next, id) {
-  var query = Idea.findById(id);
+router.post('/ideas/:idea/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.idea = req.idea;
 
-  query.exec(function (err, idea){
-    if (err) { return next(err); }
-    if (!idea) { return next(new Error('can\'t find idea')); }
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
 
-    req.idea = idea;
-    return next();
-  });
-});
+    req.idea.comments.push(comment);
+    req.idea.save(function(err, idea) {
+      if(err){ return next(err); }
 
-router.get('/ideas/:idea', function(req, res) {
-  res.json(req.idea);
-});
-
-/*upvote idea */
-router.put('/ideas/:idea/upvote', function(req, res, next) {
-  req.idea.upvote(function(err, idea){
-    if (err) { return next(err); }
-
-    res.json(idea);
+      res.json(comment);
+    });
   });
 });
 
