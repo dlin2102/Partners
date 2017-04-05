b966e1f34d7e18a277d4625eb2b901d5a96be0c0
Dan Lin <dlin2102@gmail.com>
2017-04-05 14:48:26 -0400

    finished adding users and change ideas model

--- partners/models/Ideas.js
+++ partners/models/Ideas.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var IdeaSchema = new mongoose.Schema({
   title: String,
   description: String,
+  image_url: String,
   upvotes: {type: Number, default: 0},
   comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 })
--- partners/public/javascripts/angularApp.js
+++ partners/public/javascripts/angularApp.js
@@ -1,117 +1,249 @@
 angular.module('partners', ['ui.router'])
 
-.config([
-    "$stateProvider",
-    "$urlRouterProvider",
-    RouterFunction
-])
+    .config([
+        "$stateProvider",
+        "$urlRouterProvider",
+        RouterFunction
+    ])
 
-.factory('ideas', ['$http', function($http) {
-  var i = {
-    ideas: []
-  };
-  i.getAll = function() {
-  return $http.get('/ideas').success(function(data){
-    angular.copy(data, i.ideas);
-  });
-}
-i.get = function(id) {
-  return $http.get('/ideas/' + id).then(function(res){
-    return res.data;
-  });
-};
-i.delete = function(idea){
-       return $http.delete('/ideas/' + idea._id).success(function(data){
-         angular.copy(data, i.ideas)
-       })
-     }
+    .factory('auth', ['$http', '$window', function($http, $window) {
+        var auth = {};
+        auth.saveToken = function(token) {
+            $window.localStorage['partners-token'] = token;
+        };
 
-  i.create = function(idea){
-    return $http.post('/ideas', idea).success(function(data){
-      i.ideas.push(data)
-    })
-  }
+        auth.getToken = function() {
+            return $window.localStorage['partners-token'];
+        }
+        auth.isLoggedIn = function() {
+            var token = auth.getToken();
 
-  i.upvote = function(idea) {
-  return $http.put('/ideas/' + idea._id + '/upvote')
-    .success(function(data){
-      idea.upvotes += 1;
-    });
-};
-i.addComment = function(id, comment) {
-  return $http.post('/ideas/' + id + '/comments', comment)
-}
-  return i;
-}])
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
 
-.controller('MainCtrl', [
-    '$scope',
-     'ideas',
-    function($scope, ideas) {
-        $scope.test = 'Hello world!'
-
-        $scope.ideas = ideas.ideas
-
-        $scope.addIdea = function() {
-            if ($scope.title === '') {
-                return;
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
             }
-            ideas.create({
-                title: $scope.title,
-                description: $scope.description
-            });
-            $scope.title = '';
-            $scope.description = '';
-        }
-        $scope.deleteIdea = function(idea){
-       ideas.delete(idea)
-     }
-        $scope.incrementUpvotes = function(idea) {
-            ideas.upvote(idea);
-        }
+        };
 
+        auth.currentUser = function() {
+            if (auth.isLoggedIn()) {
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.username;
+            }
+        };
+
+        auth.register = function(user) {
+            return $http.post('/register', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logIn = function(user) {
+            return $http.post('/login', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logOut = function() {
+            $window.localStorage.removeItem('partners-token');
+        };
+
+        return auth;
     }])
 
-.controller('IdeasCtrl', ['$scope',
-'ideas',
-'idea',
-function($scope, ideas, idea){
-  $scope.idea = idea;
+    .factory('ideas', ['$http', 'auth', function($http, auth) {
+        var i = {
+            ideas: []
+        };
+        i.getAll = function() {
+            return $http.get('/ideas').success(function(data) {
+                angular.copy(data, i.ideas);
+            });
+        }
+        i.get = function(id) {
+            return $http.get('/ideas/' + id).then(function(res) {
+                return res.data;
+            });
+        };
 
-  $scope.addComment = function(){
-  if($scope.body === '') { return; }
-  ideas.addComment(idea._id, {
-    body: $scope.body,
-    author: 'user',
-  }).success(function(comment) {
-    $scope.idea.comments.push(comment);
-  })
-  $scope.body = '';
-};
-}])
+        i.delete = function(idea) {
+            return $http.delete('/ideas/' + idea._id, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            }).success(function(data) {
+                angular.copy(data, i.ideas)
+            })
+        }
+
+        i.create = function(idea) {
+            return $http.post('/ideas', idea, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            }).success(function(data) {
+                i.ideas.push(data)
+            })
+        }
+
+        i.upvote = function(idea) {
+            return $http.put('/ideas/' + idea._id + '/upvote', null, {
+                headers: {
+                    Authorization: 'Bearer ' +auth.getToken()
+                }
+            }).success(function(data) {
+                idea.upvotes += 1;
+            });
+        };
+
+        i.addComment = function(id, comment) {
+            return $http.post('/ideas/' + id + '/comments', comment, {
+                    headers: {
+                        Authorization: 'Bearer ' +auth.getToken()}
+                });
+        };
+
+        return i;
+    }])
+
+    .controller('AuthCtrl', [
+        '$scope',
+        '$state',
+        'auth',
+        function($scope, $state, auth) {
+            $scope.user = {};
+
+            $scope.register = function() {
+                auth.register($scope.user).error(function(error) {
+                    $scope.error = error;
+                }).then(function() {
+                    $state.go('home');
+                });
+            };
+
+            $scope.logIn = function() {
+                auth.logIn($scope.user).error(function(error) {
+                    $scope.error = error;
+                }).then(function() {
+                    $state.go('home');
+                });
+            };
+        }
+    ])
+
+    .controller('MainCtrl', [
+        '$scope',
+        'ideas',
+        'auth',
+        function($scope, ideas, auth) {
+            $scope.test = 'Hello world!'
+
+            $scope.ideas = ideas.ideas
+            $scope.isLoggedIn = auth.isLoggedIn;
+
+            $scope.addIdea = function() {
+                if ($scope.title === '') {
+                    return;
+                }
+                ideas.create({
+                    title: $scope.title,
+                    description: $scope.description,
+                    image_url: $scope.image_url
+                });
+                $scope.title = '';
+                $scope.description = '';
+                $scope.image_url = '';
+            }
+            $scope.deleteIdea = function(idea) {
+                ideas.delete(idea)
+            }
+            $scope.incrementUpvotes = function(idea) {
+                ideas.upvote(idea);
+            }
+
+        }
+    ])
+
+    .controller('IdeasCtrl', ['$scope',
+        'ideas',
+        'idea',
+        'auth',
+        function($scope, ideas, idea, auth) {
+            $scope.idea = idea;
+            $scope.isLoggedIn = auth.isLoggedIn;
+
+            $scope.addComment = function() {
+                if ($scope.body === '') {
+                    return;
+                }
+                ideas.addComment(idea._id, {
+                    body: $scope.body,
+                    author: 'user',
+                }).success(function(comment) {
+                    $scope.idea.comments.push(comment);
+                })
+                $scope.body = '';
+            }
+        }
+    ])
+
+    .controller('NavCtrl', [
+        '$scope',
+        'auth',
+        function($scope, auth) {
+            $scope.isLoggedIn = auth.isLoggedIn;
+            $scope.currentUser = auth.currentUser;
+            $scope.logOut = auth.logOut;
+        }
+    ]);
+
 
 function RouterFunction($stateProvider, $urlRouterProvider) {
-$stateProvider
-    .state('home', {
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainCtrl',
-        resolve: {
-          postPromise: ['ideas', function(ideas){
-            return ideas.getAll();
-          }]
-        }
-      })
- .state('ideas', {
-   url: '/ideas/{id}',
-   templateUrl: '/ideas.html',
-   controller: 'IdeasCtrl',
-   resolve: {
-     idea: ['$stateParams', 'ideas', function($stateParams, ideas){
-       return ideas.get($stateParams.id);
-     }]
-   }
- });
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['ideas', function(ideas) {
+                    return ideas.getAll();
+                }]
+            }
+        })
+        .state('ideas', {
+            url: '/ideas/{id}',
+            templateUrl: '/ideas.html',
+            controller: 'IdeasCtrl',
+            resolve: {
+                idea: ['$stateParams', 'ideas', function($stateParams, ideas) {
+                    return ideas.get($stateParams.id);
+                }]
+            }
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+            }]
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: '/register.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+            }]
+        });
 
-$urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 }
